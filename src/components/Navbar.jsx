23851929955d84742d91ee2d8d9a0d83988f9f98
maxdavid <me@maxdavid.com>
2019-07-16 15:09:39 -0700

    Add functional dark mode toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useDarkMode } from '../hooks';
 
-const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+const Navbar = props => {
+  const { darkMode, setDarkMode } = props;
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
   };
+
   return (
-    <nav className="navbar">
+    <nav className='navbar'>
       <h1>Crypto Tracker</h1>
-      <div className="dark-mode__toggle">
-        <div
-          onClick={toggleMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
-        />
+      <div className='dark-mode-toggler'>
+        <div className='dark-mode-icon-container'>
+          <div id='dark-mode-icon' className={darkMode ? 'hide' : ''} />
+          <div id='light-mode-icon' className={!darkMode ? 'hide' : ''} />
+        </div>
+        <div onClick={toggleMode} className='dark-mode__toggle'>
+          <div className={darkMode ? 'toggle toggled' : 'toggle'} />
+        </div>
       </div>
     </nav>
   );
--- src/index.js
+++ src/index.js
@@ -1,30 +1,39 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-import Charts from "./components/Charts";
-import Navbar from "./components/Navbar";
+import Charts from './components/Charts';
+import Navbar from './components/Navbar';
+import { useDarkMode } from './hooks';
 
-import "./styles.css";
+import './styles.css';
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
+  const [darkMode, setDarkMode] = useDarkMode();
 
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true'
       )
       .then(res => setCoinData(res.data))
       .catch(err => console.log(err));
   }, []);
+
+  useEffect(() => {
+    const element = window.document.body;
+    if (darkMode) element.classList.add('dark-mode');
+    else element.classList.remove('dark-mode');
+  }, [darkMode]);
+
   return (
-    <div className="App">
-      <Navbar />
+    <div className='App'>
+      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       <Charts coinData={coinData} />
     </div>
   );
 };
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
